9da709205e76b4f243cf2c9d9ab8de692c74d927
Leonel <leon.alberto4502@gmail.com>
2024-12-23 20:17:28 -0300

    Update server.js for vercel

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,25 +4,6 @@ import authRouter from "../routes/auth.routes.js";
 import userRouter from "../routes/user.routes.js";
 import postRouter from "../routes/post.routes.js";
 import { PORT } from "./config.js";
-import { logger } from "./logger.js";
-
-// Middleware de winston para guardar solicitudes y respuestas globales
-app.use((req, res, next) => {
-  res.on("finish", () => {
-    //este registra cualquier solicitud
-    // logger.info(
-    //   `${req.method} ${req.url} - status: ${res.statusCode}`
-    // );
-
-    //registra solo errores
-    if (res.statusCode >= 500) {
-      logger.error(
-        `${req.method} ${req.url} - status: ${res.statusCode}`
-      );
-    }
-  });
-  next();
-});
 
 connectDB();
 
@@ -30,13 +11,10 @@ app.use('/auth', authRouter)
 app.use('/user', userRouter)
 app.use('/post', postRouter)
 
-// Inicio del servidor
 app.listen(PORT, () => {
-  logger.info(`Servidor corriendo en: http://localhost:${PORT}`);
-});
-
-// Ruta principal
-app.get("/", (req, res) => {
-  res.json("¡Bienvenido al backend!");
-});
-
+    console.log(`Server corriendo en: http://localhost:${PORT}`);
+  });
+  
+  app.get("/", (req, res) => {
+    res.json("¡Bienvenido al backend!");
+  });
